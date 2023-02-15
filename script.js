80ebc5cca6d1adf57147571b3dc00c920d636be3
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-02-15 13:17:24 +0000

    pushed updated file

--- script.js
+++ script.js
@@ -179,11 +179,10 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector(getRandom(pwdOptionsObj));
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
